76ad314f31b4972b97516bae089e909805f063b3
Reza Radfar <reza.radfar1306@gmail.com>
2022-04-02 03:33:35 +1100

    PRDB_2: adds the apiResources for category and product with route setting

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Resources\CategoryResource;
+use App\Http\Resources\ProductResource;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +19,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/products' , function () {
+
+        $products = Product::orderBy('name')->get();
+        return ProductResource::collection($products);
+});
+
+Route::get('/products/{product}' , function (Product $product) {
+
+    return new ProductResource($product);
+
+});
+
+Route::get('/categories', function () {
+
+    $categories = Category::orderBy('name')->get();
+    return CategoryResource::collection($categories);
 });
